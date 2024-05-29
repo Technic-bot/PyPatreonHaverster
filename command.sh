737d7b4475b02427c0c27595ca928bc1086f50c8
Technic_bot <technic.robot.35@gmail.com>
2024-05-28 21:54:17 -0600

    Add loging command

--- command.sh
+++ command.sh
@@ -1,1 +1,4 @@
 python3.9 /home/username/PyPatreonHaverster/patreonharvester/main.py https://www.patreon.com/twokinds/posts /u01/vault/ --gecko-path /usr/local/bin/geckodriver --db-file //home/armorclad/ArtVault/artvault/db/twokinds_patreon.db --browser-dir /home/username/PyPatreonHaverster/selly --post-process thumbnail --post-process-dir /u01/vault/thumbnails/
+
+python3.9 /home/username/PyPatreonHaverster/patreonharvester/main.py https://www.patreon.com/twokinds/posts /u01/vault/ --gecko-path /usr/local/bin/geckodriver --db-file //home/armorclad/ArtVault/artvault/db/twokinds_patreon.db --browser-dir /home/username/PyPatreonHaverster/selly --post-process thumbnail --post-process-dir /u01/vault/thumbnails/ --login
+
